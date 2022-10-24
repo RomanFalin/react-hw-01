89bf7a1a6caac077cfc155788e135ab3480a0b10
RomanFalin <romanfalin01@gmail.com>
2022-10-24 17:06:18 +0200

    final markup

--- src/components/TransactionHistory.jsx
+++ src/components/TransactionHistory.jsx
@@ -1,23 +1,25 @@
 const TransactionHistory = ({ items }) => {
-  <table class="transaction-history">
-    <thead>
-      <tr>
-        <th>Type</th>
-        <th>Amount</th>
-        <th>Currency</th>
-      </tr>
-    </thead>
-
-    <tbody>
-      {items.map((id, type, amount, currencies) => (
-        <tr key={id}>
-          <td>{type}</td>
-          <td>{amount}</td>
-          <td>{currencies}</td>
+  return (
+    <table class="transaction-history">
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
         </tr>
-      ))}
-    </tbody>
-  </table>;
+      </thead>
+
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => (
+          <tr key={id}>
+            <td>{type}</td>
+            <td>{amount}</td>
+            <td>{currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 };
 
 export default TransactionHistory;
